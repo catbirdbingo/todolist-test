93bb83f5f4ed00bee1f794d9323379c555884443
catbirdbingo <daniil.bakhtyukov@gmail.com>
2018-02-15 19:52:07 +0300

    added project controller

---
+++ app/controllers/project_controller.rb
@@ -0,0 +1,7 @@
+class ProjectController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'project/index'
+
+  get 'project/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :projects
+resources :todos
+
 end
